5c066ef09e4087e6540aa1869f08ff1b05c08686
AppleShield <clement.rama13@gmail.com>
2026-05-30 22:36:24 +0200

    Descriptive data mean, variance, correlation

---
+++ Descriptive_data.R
@@ -0,0 +1,24 @@
+library(dplyr)
+library(sf)
+library(ggplot2)
+library(rnaturalearth)
+
+data <- read.csv("C:/Users/cleme/Documents/Master_Allemagne/Topic_Economic_History/Dataset_code/data_set_2010_allvar.csv")
+
+##### mean, variance, std################"
+
+cat("GDL Vulnerability Index:", mean(data$gvi, na.rm = TRUE), "\n")
+cat("Comprehensive Subnational Corruption Index:", mean(data$fullsci, na.rm = TRUE), "\n")
+cat("Mean years of education of adults aged 25+:", mean(data$edyr25, na.rm = TRUE), "\n")
+cat("Population in millions:", mean(data$regpopm, na.rm = TRUE), "\n")
+cat("Household size:", mean(data$hhsize, na.rm = TRUE), "\n")
+cat("International Wealth Index:", mean(data$iwi, na.rm = TRUE), "\n")
+
+cat(" Variance GDL Vulnerability Index:", var(data$gvi, na.rm = TRUE), "\n")
+cat(" VarianceComprehensive Subnational Corruption Index:", var(data$fullsci, na.rm = TRUE), "\n")
+cat(" Variance Mean years of education of adults aged 25+:", var(data$edyr25, na.rm = TRUE), "\n")
+cat(" Variance Population in millions:", var(data$regpopm, na.rm = TRUE), "\n")
+cat(" Variance Household size:", var(data$hhsize, na.rm = TRUE), "\n")
+cat(" Variance International Wealth Index:", var(data$iwi, na.rm = TRUE), "\n")
+
+
