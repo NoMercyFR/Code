6ad961e0687feb41c0da799c3211c90645ce77f6
AppleShield <clement.rama13@gmail.com>
2026-06-02 15:23:14 +0200

    New descriptive data with all the variables !

--- Descriptive_data.R
+++ Descriptive_data.R
@@ -3,31 +3,25 @@ library(sf)
 library(ggplot2)
 library(rnaturalearth)
 
-data <- read.csv("C:/Users/cleme/Documents/Master_Allemagne/Topic_Economic_History/Dataset_code/data_set_2010_allvar.csv")
+data <- read.csv("C:/Users/cleme/Documents/Master_Allemagne/Topic_Economic_History/Dataset_code/final_data/GDL_2010_merged_common_rows.csv")
 
 ##### mean, variance, std################"
-"""
-cat("GDL Vulnerability Index:", mean(data$gvi, na.rm = TRUE), "\n")
-cat("Comprehensive Subnational Corruption Index:", mean(data$fullsci, na.rm = TRUE), "\n")
-cat("Mean years of education of adults aged 25+:", mean(data$edyr25, na.rm = TRUE), "\n")
-cat("Population in millions:", mean(data$regpopm, na.rm = TRUE), "\n")
-cat("Household size:", mean(data$hhsize, na.rm = TRUE), "\n")
-cat("International Wealth Index:", mean(data$iwi, na.rm = TRUE), "\n")
 
-cat(" Variance GDL Vulnerability Index:", var(data$gvi, na.rm = TRUE), "\n")
-cat(" VarianceComprehensive Subnational Corruption Index:", var(data$fullsci, na.rm = TRUE), "\n")
-cat(" Variance Mean years of education of adults aged 25+:", var(data$edyr25, na.rm = TRUE), "\n")
-cat(" Variance Population in millions:", var(data$regpopm, na.rm = TRUE), "\n")
-cat(" Variance Household size:", var(data$hhsize, na.rm = TRUE), "\n")
-cat(" Variance International Wealth Index:", var(data$iwi, na.rm = TRUE), "\n")
-"""
-#data %>%
-#  summarise(across(where(is.numeric), list(mean = ~mean(., na.rm = TRUE), 
-#                                           var = ~var(., na.rm = TRUE))))
+
+cat("Variance Comprehensive Subnational Corruption Index:", var(data$fullsci, na.rm = TRUE), "\n")
+cat("Variance Mean years of schooling:", var(data$msch, na.rm = TRUE), "\n")
+cat("Variance Population in millions:", var(data$popshare, na.rm = TRUE), "\n")
+cat("Variance Household size:", var(data$hhsize, na.rm = TRUE), "\n")
+cat("Variance International Wealth Index:", var(data$iwi, na.rm = TRUE), "\n")
+cat("Variance Percentage of People living urban:", var(data$urban, na.rm = TRUE), "\n")
+cat("Variance Gini coefficient:", var(data$gini, na.rm = TRUE), "\n")
+cat("Variance Infant Mortality:", var(data$infmort, na.rm = TRUE), "\n")
+cat("Variance Piped water:", var(data$pipedwater, na.rm = TRUE), "\n")
+cat("Variance Electricity:", var(data$electr, na.rm = TRUE), "\n")
 
 ####### Covariance matrix ##############
 
-columns <- c("gvi","fullsci","edyr25", "regpopm","iwi","hhsize")
+columns <- c("fullsci", "msch", "popshare","hhsize","iwi", "urban" , "gini" , "infmort" ,"pipedwater" ,"electr")
 
 covariance_matrix <- cov(data[columns], use = "complete.obs")
 correlation_matrix <- cor(data[columns], use = "complete.obs")
